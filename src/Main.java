0e2f0444a64aa3231701300f07a56d08dde4450c
NicolaeRazvan <caramarauzer@gmail.com>
2021-10-21 21:36:16 +0300

    Tema finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //Cerinta 1
         System.out.println("Hello \n" + "Razvan");
+        //Cerinta 2
+        System.out.println(5 + 4);
+        //Cerinta 3
+        System.out.println(10 / 5);
+        //Cerinta 4 a)
+        System.out.println(-5 +8 * 6);
+        //Cerinta 4 b)
+        System.out.println((55+9) % 9);
+        //Cerinta 4 c)
+        System.out.println(20 + -3 * 5 / 8);
+        //Cerinta 4 d)
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
     }
 }
